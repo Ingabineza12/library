98592d8fdc7940cadad4224e2d15006781984857
Deborah M Ingabire <ingabineza@gmail.com>
2021-02-09 13:01:52 +0200

    added urls to use

---
+++ libraryApp/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import url,include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns=[
+    url(r'^$', views.index, name='home'),
+    url(r'^book/$', views.view_books, name='view_books'),
+    url(r'^book/add/$', views.add_book, name='add_book'),
+    url(r'^delete_book/$', views.ViewDeletePost, name='delete_book'),
+    url(r'^edit/profile$',  views.profile_edit,name='profile_edit'),
+    url(r'^myprofile/$',  views.profilemy,name='profilemy'),
+    url(r'^issue/$', views.view_issue, name='view_issue'),
+    url(r'^issue/new/$', views.new_issue, name='new_issue'),
+    url(r'^search/$',  views.search_books,name='search_books'),
+
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
